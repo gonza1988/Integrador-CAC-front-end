0b2b4d5feabc1ac6993c89d7e51cac32758db204
Gonzalo Cozzo <gonza007_88@hotmail.com>
2023-05-28 22:42:35 -0300

    terminando funcionalidad de tickets con sus distintas categorias de precios y funcion borrar

--- assets/js/tickets.js
+++ assets/js/tickets.js
@@ -1,10 +1,28 @@
 const valorTicket = 200;
 
 function calcularTicket() {
-    
-    alert(leerCantidad());
-    alert(leerCategoria());
-    anotarValor(500);
+
+    let valor = 0;
+    cantidad = leerCantidad();
+    if (cantidad === '' || cantidad == 0) {
+        error();
+    } else {
+        limpiarError();
+        switch (leerCategoria()) {
+            case '1':
+                valor = (valorTicket - valorTicket * 80 / 100) * cantidad;
+                break;
+            case '2':
+                valor = (valorTicket - valorTicket * 50 / 100) * cantidad;
+                break;
+            case '3':
+                valor = (valorTicket - valorTicket * 15 / 100) * cantidad;
+                break;
+            default:
+                valor = 0;    
+        }
+        anotarValor(valor);
+    }
 }
 
 function leerCantidad() {
@@ -19,4 +37,22 @@ function leerCategoria() {
 
 function anotarValor(monto) {
     document.getElementById('monto').innerHTML = 'Total a Pagar: $ ' + monto;
+}
+
+function error() {
+    document.getElementById('advTickets').style.display = 'block';
+    document.getElementById('monto').innerHTML = 'Total a Pagar: $ ';
+}
+
+function limpiarError() {
+    document.getElementById('advTickets').style.display = 'none';
+}
+
+function borrar() {
+
+    document.getElementById('exampleName').value = '';
+    document.getElementById('exampleSurname').value = '';
+    document.getElementById('exampleEmail').value = '';
+    document.getElementById('exampleCant').value = '';
+    document.getElementById('monto').innerHTML = 'Total a Pagar: $ ';
 }
